9898dafadfe8a5e713a9c39d02f6e82c98a1c3cf
yoann <yrebah@webnet.fr>
2024-05-28 19:05:04 +0200

    init

--- app/dist/js/index.js
+++ app/dist/js/index.js
@@ -1,3 +1,33 @@
+class LocalStorageController {
+
+    static get Data() {
+        const data = JSON.parse(localStorage.getItem('portfolio-yr'));
+        return data;
+    }
+
+    static SetData(key, value) {
+        const data = LocalStorageController.Data;
+        data[key] = value;
+        const stringifiedData = JSON.stringify(data);
+        localStorage.setItem('portfolio-yr', stringifiedData);
+    }
+
+    init() {
+        if(localStorage.getItem('portfolio-yr') == null) this.StoreDefaultData();
+    }
+
+    StoreDefaultData() {
+        const defaultData = {
+            mainNavIsReduce: false,
+            skillsIsReduce: false,
+            projectsIsReduce: false,
+            roadmapIsReduce: false
+        }
+
+        localStorage.setItem('portfolio-yr', JSON.stringify(defaultData));
+    }
+}
+
 class ContentController {
 
     #dataH2 = document.querySelectorAll('[data-h2]');
@@ -6,7 +36,7 @@ class ContentController {
         this.AddEventClickH2();
     }
 
-    TogglePseudoCssH2(elem) {
+    ToggleStyleH2(elem) {
         if(elem) {
             if(elem.classList.contains('reduce')) {
                 elem.classList.remove('reduce');
@@ -19,6 +49,7 @@ class ContentController {
     ToggleContent(dataContent) {
         if(document.querySelector(`[data-content=${dataContent}]`)) {
             const content = document.querySelector(`[data-content=${dataContent}]`);
+
             if(content.classList.contains('display-none')) {
                 content.classList.remove('display-none');
             } else {
@@ -34,11 +65,12 @@ class ContentController {
                     const thisTarget = e.target;
                     const thisDataH2 = thisTarget.getAttribute('data-h2');
                     this.ToggleContent(thisDataH2);
-                    this.TogglePseudoCssH2(thisTarget);
+                    this.ToggleStyleH2(thisTarget);
                 })
             });
         }
     }
+
 }
 
 class HeroBannerController {
@@ -46,11 +78,11 @@ class HeroBannerController {
     #heroBanner = document.querySelector('[data-container="hero-banner"]');
     #mainNav = document.querySelector('[data-container="main-nav"]');
     #btnReduceMainNav = document.querySelector('[data-btn-action="reduce-main-nav"]');
-    #iconBtnReduceMainNav = this.#btnReduceMainNav.querySelector('.fa-icon');
 
     init() {
         this.AddEventWindowScroll();
         this.AddEventClickBtnReduceMainNav();
+        this.ToggleMainNavByLocalData();
     }
 
     AddEventWindowScroll() {
@@ -76,8 +108,12 @@ class HeroBannerController {
         if(this.#mainNav) {
             if(this.#mainNav.classList.contains('reduce')) {
                 this.#mainNav.classList.remove('reduce');
+                this.#btnReduceMainNav.setAttribute('title', 'Masquer le menu');
+                LocalStorageController.SetData('mainNavIsReduce', false);
             } else {
                 this.#mainNav.classList.add('reduce');
+                this.#btnReduceMainNav.setAttribute('title', 'Afficher le menu');
+                LocalStorageController.SetData('mainNavIsReduce', true);
             }
         }
     }
@@ -89,9 +125,24 @@ class HeroBannerController {
             })
         }
     }
+
+    ToggleMainNavByLocalData() {
+        const localData = LocalStorageController.Data;
+        
+        if(localData.mainNavIsReduce === true) {
+            this.#mainNav.classList.add('reduce');
+            this.#btnReduceMainNav.setAttribute('title', 'Afficher le menu');
+        } else {
+            this.#mainNav.classList.remove('reduce');
+            this.#btnReduceMainNav.setAttribute('title', 'Masquer le menu');
+        }
+    }
 }
 
 window.onload = () => {
+
+    const localStorageController = new LocalStorageController();
+    localStorageController.init();
    
     const contentController = new ContentController();
     contentController.init();
