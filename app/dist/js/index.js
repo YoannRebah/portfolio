d82295ad705ed91235114fe62bcff0354253555d
yoann <yrebah@webnet.fr>
2024-05-28 15:39:21 +0200

    init

--- app/dist/js/index.js
+++ app/dist/js/index.js
@@ -28,14 +28,16 @@ class ContentController {
     }
 
     AddEventClickH2() {
-        this.#dataH2.forEach((elem)=>{
-            elem.addEventListener('click', (e)=>{
-                const thisTarget = e.target;
-                const thisDataH2 = thisTarget.getAttribute('data-h2');
-                this.ToggleContent(thisDataH2);
-                this.TogglePseudoCssH2(thisTarget);
-            })
-        })
+        if(document.querySelector('[data-h2]')) {
+            this.#dataH2.forEach((elem)=>{
+                elem.addEventListener('click', (e)=>{
+                    const thisTarget = e.target;
+                    const thisDataH2 = thisTarget.getAttribute('data-h2');
+                    this.ToggleContent(thisDataH2);
+                    this.TogglePseudoCssH2(thisTarget);
+                })
+            });
+        }
     }
 }
 
@@ -43,9 +45,12 @@ class HeroBannerController {
 
     #heroBanner = document.querySelector('[data-container="hero-banner"]');
     #mainNav = document.querySelector('[data-container="main-nav"]');
+    #btnReduceMainNav = document.querySelector('[data-btn-action="reduce-main-nav"]');
+    #iconBtnReduceMainNav = this.#btnReduceMainNav.querySelector('.fa-icon');
 
     init() {
         this.AddEventWindowScroll();
+        this.AddEventClickBtnReduceMainNav();
     }
 
     AddEventWindowScroll() {
@@ -53,14 +58,36 @@ class HeroBannerController {
             const scrollY = window.scrollY;
             const opacityValue = scrollY/250;
 
-            this.#heroBanner.style.opacity = `${1 - opacityValue}`;
-
-            if(scrollY > 180) {
-                this.#mainNav.style.opacity = `1`;
-            } else {
-                this.#mainNav.style.opacity = `0`;
+            if(this.#heroBanner) {
+                this.#heroBanner.style.opacity = `${1 - opacityValue}`;
             }
-        })
+
+            if(this.#mainNav) {
+                if(scrollY > 80) {
+                    this.#mainNav.style.opacity = `1`;
+                } else {
+                    this.#mainNav.style.opacity = `0`;
+                }
+            }
+        });
+    }
+
+    ToggleMainNav() {
+        if(this.#mainNav) {
+            if(this.#mainNav.classList.contains('reduce')) {
+                this.#mainNav.classList.remove('reduce');
+            } else {
+                this.#mainNav.classList.add('reduce');
+            }
+        }
+    }
+
+    AddEventClickBtnReduceMainNav() {
+        if(this.#btnReduceMainNav) {
+            this.#btnReduceMainNav.addEventListener('click', ()=>{
+                this.ToggleMainNav();
+            })
+        }
     }
 }
 
