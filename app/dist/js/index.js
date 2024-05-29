983a85ba5fc74aaef338f7eac25128c2a354d2ce
yoann <yrebah@webnet.fr>
2024-05-29 15:11:35 +0200

    add vhs effect

--- app/dist/js/index.js
+++ app/dist/js/index.js
@@ -170,6 +170,47 @@ class TimerController {
     }
 }
 
+class OverlayController {
+    #overlay = document.querySelector('.overlay');
+
+    init() {
+        this.AddAndRemoveRandomDivs();
+        this.StartPeriodicRemoval();
+    }
+
+    AddAndRemoveRandomDivs() {
+        const addDivs = () => {
+            const numDivsToAdd = Math.floor(Math.random() * 8);
+            for (let i = 0; i < numDivsToAdd; i++) {
+                const delay = Math.random() * 16000;
+
+                setTimeout(() => {
+                    const newDiv = document.createElement('div');
+                    newDiv.className = 'glitch-line';
+                    newDiv.style.height = `${Math.floor(Math.random() * 3) + 1}px`;
+                    this.#overlay.appendChild(newDiv);
+                }, delay);
+            }
+        };
+
+        setInterval(addDivs, 16000);
+    }
+
+    RemoveAllGlitchLines() {
+        const glitchDivs = this.#overlay.querySelectorAll('.glitch-line');
+
+        glitchDivs.forEach(div => {
+            this.#overlay.removeChild(div);
+        });
+    }
+
+    StartPeriodicRemoval() {
+        setInterval(() => {
+            this.RemoveAllGlitchLines();
+        }, 6000);
+    }
+}
+
 window.onload = () => {
 
     const localStorageController = new LocalStorageController();
@@ -180,6 +221,9 @@ window.onload = () => {
 
     const mainNavController = new MainNavController();
     mainNavController.init();
+
+    const overlayController = new OverlayController();
+    overlayController.init();
 
     const timerController = new TimerController();
 
