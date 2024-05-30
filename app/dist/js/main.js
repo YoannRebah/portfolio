3406f1d047be3f58691cdbcb293bb985e3faadc3
yoann <yrebah@webnet.fr>
2024-05-30 15:00:28 +0200

    fix js

--- app/dist/js/main.js
+++ app/dist/js/main.js
@@ -46,12 +46,125 @@ class LocalStorageController {
     }
 }
 
-class ContentController {
+class SectionController {
 
     #titlesH2 = document.querySelectorAll('[data-h2]');
-
+    #contentSkills = document.querySelector('.content[data-content="skills"]');
+    #contentTools = document.querySelector('.content[data-content="tools"]');
+    #skills = [
+        {
+            id: "html",
+            label: "HTML",
+            iconClassName: "fa-brands fa-html5",
+            items: [
+                {
+                    label: "Structure",
+                    percent: 92
+                },
+                {
+                    label: "Accessibilité",
+                    percent: 84
+                },
+                {
+                    label: "SEO",
+                    percent: 77
+                },
+                {
+                    label: "Performances",
+                    percent: 99
+                },
+                {
+                    label: "Bonnes pratiques",
+                    percent: 87
+                }
+            ]
+        },
+        {
+            id: "sass",
+            label: "SASS",
+            iconClassName: "fa-brands fa-sass",
+            items: [
+                {
+                    label: "Syntaxe",
+                    percent: 80
+                },
+                {
+                    label: "Sélecteurs",
+                    percent: 90
+                },
+                {
+                    label: "Architecture",
+                    percent: 85
+                },
+                {
+                    label: "Performances",
+                    percent: 78
+                },
+                {
+                    label: "Bonnes pratiques",
+                    percent: 93
+                }
+            ]
+        },
+        {
+            id: "js",
+            label: "JavaScript",
+            iconClassName: "fa-brands fa-js",
+            items: [
+                {
+                    label: "Bases",
+                    percent: 95
+                },
+                {
+                    label: "Structure de données",
+                    percent: 88
+                },
+                {
+                    label: "POO",
+                    percent: 82
+                },
+                {
+                    label: "Manipulation du DOM",
+                    percent: 76
+                },
+                {
+                    label: "Bonnes pratiques",
+                    percent: 97
+                }
+            ]
+        }
+    ];
+    #tools = [
+        {
+            label: "VS Code",
+            animationDelay: "10s"
+        },
+        {
+            label: "Visual Studio",
+            animationDelay: "2.2s"
+        },
+        {
+            label: "Figma",
+            animationDelay: "8s"
+        },
+        {
+            label: "Git",
+            animationDelay: "4.5s"
+        },
+        {
+            label: "AEM",
+            animationDelay: "1s"
+        },
+        {
+            label: "Wordpress",
+            animationDelay: "3.8s"
+        }
+    ];
+    
     init() {
         this.AddEventClickH2();
+        this.AppendSkillsHTML();
+        this.AppendToolsHTML();
     }
 
     ToggleStyleH2(elem) {
@@ -87,6 +200,71 @@ class ContentController {
                 })
             });
         }
+    }
+
+    AppendSkillsHTML() {
+        this.#skills.forEach((elem)=>{
+            const rowSkill = document.createElement('div');
+            rowSkill.classList.add('row-skill');
+            rowSkill.setAttribute('data-skill', elem.id);
+
+            const hr = document.createElement('hr');
+            hr.classList.add('cyan');
+
+            const containerIconText = document.createElement('div');
+            containerIconText.classList.add('container-icon-text');
+
+            const label = document.createElement('p');
+            label.innerHTML = elem.label;
+
+            const icon = document.createElement('i');
+            icon.className = elem.iconClassName;
+
+            const wrapper = document.createElement('div');
+            wrapper.classList.add('wrapper');
+
+            containerIconText.append(label, icon);
+            rowSkill.append(containerIconText);
+
+            elem.items.forEach((item)=>{
+                const skillItem = document.createElement('div');
+                skillItem.classList.add('skill-item');
+
+                const label = document.createElement('p');
+                label.innerHTML = `${item.label}&nbsp;:`;
+
+                const containerProgressBar = document.createElement('div');
+                containerProgressBar.classList.add('container-progress-bar');
+
+                const progressBar = document.createElement('div');
+                progressBar.classList.add('progress-bar');
+                progressBar.style.width = `${item.percent}%`;
+                progressBar.setAttribute('data-percent', `${item.percent}%`);
+
+                containerProgressBar.append(progressBar);
+                skillItem.append(label, containerProgressBar);
+                wrapper.append(skillItem);
+            })
+
+            rowSkill.append(wrapper);
+            this.#contentSkills.append(rowSkill, hr);
+        });
+    }
+
+    AppendToolsHTML() {
+        this.#tools.forEach((elem)=>{
+            const tool = document.createElement('div');
+            tool.classList.add('tool');
+
+            const screen = document.createElement('div');
+            screen.classList.add('screen');
+            screen.setAttribute('data-tool', elem.label);
+            screen.style.animationDelay = elem.animationDelay;
+
+            tool.append(screen);
+
+            this.#contentTools.append(tool);
+        });
     }
 
 }
@@ -220,8 +398,8 @@ window.onload = () => {
     const localStorageController = new LocalStorageController();
     localStorageController.init();
    
-    const contentController = new ContentController();
-    contentController.init();
+    const sectionController = new SectionController();
+    sectionController.init();
 
     const mainNavController = new MainNavController();
     mainNavController.init();
