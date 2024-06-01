2f91229b2aa0ce9e4ec8210281d3cb784978b657
yoann <yrebah@webnet.fr>
2024-06-01 10:51:56 +0200

    fix overlay

--- app/dist/js/components/References.js
+++ app/dist/js/components/References.js
@@ -3,8 +3,24 @@ class References {
     #referencesList = document.querySelector('.references-list');
     #references = [
         {
+            html: "Miami Vice (Pilote de 1980)",
+            href: "https://x.com/WilliamReymond/status/1728148302494150986?t=n3qVBPyxhhFbx1BkEmAgYQ&s=19"
+        },
+        {
+            html: "Employee Card",
+            href: "https://www.google.com/search?q=employee+card+1980&sca_esv=470f2cd7629ffe1b&sca_upv=1&rlz=1C1ONGR_fr__1099__1099&udm=2&biw=1920&bih=911&sxsrf=ADLYWILB0iesC9XMdrWEn_ABfyuWyTrglQ%3A1717138237228&ei=PXNZZtm4Db2bkdUP3uuTkAg&ved=0ahUKEwiZy6O1preGAxW9TaQEHd71BIIQ4dUDCBA&uact=5&oq=employee+card+1980&gs_lp=Egxnd3Mtd2l6LXNlcnAiEmVtcGxveWVlIGNhcmQgMTk4MEibTVAAWJ1IcAB4AJABAJgBeaAB_guqAQQxNS4zuAEDyAEA-AEBmAIOoALfCcICBBAjGCfCAgUQABiABMICCxAAGIAEGLEDGIMBwgIIEAAYgAQYsQPCAg0QABiABBixAxhDGIoFwgIKEAAYgAQYQxiKBcICBBAAGB7CAgcQABiABBgTmAMAkgcEMTEuM6AH40s&sclient=gws-wiz-serp#vhid=IKqhJ5HYQ4lS3M&vssid=mosaic"
+        },
+        {
             html: "3D Hover Effect",
             href: "https://codepen.io/technokami/pen/abojmZa"
+        },
+        {
+            html: "Neon Text Effect",
+            href: "https://codepen.io/ananyaneogi/pen/Bgozrz"
+        },
+        {
+            html: "Marquee",
+            href: "https://ryanmulligan.dev/blog/css-marquee/"
         },
         {
             html: "Wave Generator",
@@ -19,20 +35,8 @@ class References {
             href: "https://fonts.google.com/"
         },
         {
-            html: "Employee Card",
-            href: "https://www.google.com/search?q=employee+card+1980&sca_esv=470f2cd7629ffe1b&sca_upv=1&rlz=1C1ONGR_fr__1099__1099&udm=2&biw=1920&bih=911&sxsrf=ADLYWILB0iesC9XMdrWEn_ABfyuWyTrglQ%3A1717138237228&ei=PXNZZtm4Db2bkdUP3uuTkAg&ved=0ahUKEwiZy6O1preGAxW9TaQEHd71BIIQ4dUDCBA&uact=5&oq=employee+card+1980&gs_lp=Egxnd3Mtd2l6LXNlcnAiEmVtcGxveWVlIGNhcmQgMTk4MEibTVAAWJ1IcAB4AJABAJgBeaAB_guqAQQxNS4zuAEDyAEA-AEBmAIOoALfCcICBBAjGCfCAgUQABiABMICCxAAGIAEGLEDGIMBwgIIEAAYgAQYsQPCAg0QABiABBixAxhDGIoFwgIKEAAYgAQYQxiKBcICBBAAGB7CAgcQABiABBgTmAMAkgcEMTEuM6AH40s&sclient=gws-wiz-serp#vhid=IKqhJ5HYQ4lS3M&vssid=mosaic"
-        },
-        {
-            html: "Marquee",
-            href: "https://ryanmulligan.dev/blog/css-marquee/"
-        },
-        {
             html: "Unsplash",
             href: "https://unsplash.com/fr"
-        },
-        {
-            html: "Neon Text Effect",
-            href: "https://codepen.io/ananyaneogi/pen/Bgozrz"
         }
     ];
 
