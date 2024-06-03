92b281491673a170f2effbafcea5e15996e4912e
yoann <yrebah@webnet.fr>
2024-06-03 12:10:16 +0200

    feat video games

--- app/dist/js/components/Arcade.js
+++ app/dist/js/components/Arcade.js
@@ -58,26 +58,41 @@ class Arcade {
         });
     }
 
-    UpdateGameMouseCursor() {
-        this.#game.classList.forEach((className) => {
-            if (className.startsWith('toto')) {
-                this.#game.classList.remove(className);
-            }
-        });
+    UpdateGameMouseCursor(direction /* left or right */) {
+        this.#game.classList.remove('game-cursor-up');
+        this.#game.classList.add(`game-cursor-${direction}`);
+        let timeout = setTimeout(()=>{
+            this.#game.classList.remove(`game-cursor-${direction}`);
+            this.#game.classList.add('game-cursor-up');
+            clearTimeout(timeout);
+        }, 500)
     }
 
     AddEventMouseMoveGame() {
         this.#game.addEventListener('mousemove', (e) => {
             const clientX = e.clientX;
-
+            const fixedY = this.#game.offsetHeight - 100; // Position verticale fixe à 100px du bas du conteneur
+    
             if (clientX < this.#currentPositionX) {
+                this.UpdateGameMouseCursor('left');
                 console.log('less');
             } else if (clientX > this.#currentPositionX) {
+                this.UpdateGameMouseCursor('right');
                 console.log('more');
             }
-
+    
             this.#currentPositionX = clientX;
-            console.log(clientX);
+    
+            // Mettre à jour la position du curseur en forcant la position Y
+            this.UpdateCursorPosition(clientX, fixedY);
+            console.log(`clientX: ${clientX}, fixedY: ${fixedY}`);
         });
+    }
+    
+    UpdateCursorPosition(x, y) {
+        const cursor = document.querySelector('.cursor'); // Assurez-vous d'avoir un élément de curseur
+        const cursorWidth = cursor.offsetWidth;
+        cursor.style.left = `${x - cursorWidth / 2}px`; // Ajustement de la position en soustrayant la moitié de la largeur
+        cursor.style.top = `${y}px`;
     }
 }
