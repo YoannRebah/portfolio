38bd93f3337733f0ec99053491c177bebdb81692
yoann <yrebah@webnet.fr>
2024-06-03 14:09:23 +0200

    feat video games

--- app/dist/js/components/Arcade.js
+++ app/dist/js/components/Arcade.js
@@ -7,12 +7,18 @@ class Arcade {
     #videoGame = document.querySelector('.video-game');
     #game = document.querySelector('.game');
     #currentPositionX = 0;
+    #gameCursor;
 
     init() {
         this.AddEventBtnOpenMenu();
         this.AddEventBtnNewGame();
         this.AddEventBtnQuitMenu();
+        this.AddEventMouseEnterGame();
         this.AddEventMouseMoveGame();
+    }
+
+    get GameWidth() {
+        return this.#game.offsetWidth;
     }
 
     ShowArcadeMenu() {
@@ -59,40 +65,45 @@ class Arcade {
     }
 
     UpdateGameMouseCursor(direction /* left or right */) {
-        this.#game.classList.remove('game-cursor-up');
-        this.#game.classList.add(`game-cursor-${direction}`);
-        let timeout = setTimeout(()=>{
-            this.#game.classList.remove(`game-cursor-${direction}`);
-            this.#game.classList.add('game-cursor-up');
-            clearTimeout(timeout);
-        }, 500)
+        if(document.querySelector('.game-cursor')) {
+            this.#gameCursor.classList.remove('game-cursor-up');
+            this.#gameCursor.classList.add(`game-cursor-${direction}`);
+            let timeout = setTimeout(()=>{
+                this.#gameCursor.classList.remove(`game-cursor-${direction}`);
+                this.#gameCursor.classList.add('game-cursor-up');
+                clearTimeout(timeout);
+            }, 500)
+        }
+    }
+
+    AddEventMouseEnterGame() {
+        this.#game.addEventListener('mouseenter', (e) => {
+            if(document.querySelector('.game-cursor')) {
+                return
+            } else {
+                this.#gameCursor = document.createElement('div');
+                this.#gameCursor.classList.add('game-cursor', 'game-cursor-up')
+                this.#game.append(this.#gameCursor);
+            }
+        });
     }
 
     AddEventMouseMoveGame() {
         this.#game.addEventListener('mousemove', (e) => {
             const clientX = e.clientX;
-            const fixedY = this.#game.offsetHeight - 100; // Position verticale fixe à 100px du bas du conteneur
     
             if (clientX < this.#currentPositionX) {
                 this.UpdateGameMouseCursor('left');
-                console.log('less');
             } else if (clientX > this.#currentPositionX) {
                 this.UpdateGameMouseCursor('right');
-                console.log('more');
             }
     
             this.#currentPositionX = clientX;
-    
-            // Mettre à jour la position du curseur en forcant la position Y
-            this.UpdateCursorPosition(clientX, fixedY);
-            console.log(`clientX: ${clientX}, fixedY: ${fixedY}`);
+            this.UpdateCursorPosition(this.#currentPositionX);
         });
     }
     
-    UpdateCursorPosition(x, y) {
-        const cursor = document.querySelector('.cursor'); // Assurez-vous d'avoir un élément de curseur
-        const cursorWidth = cursor.offsetWidth;
-        cursor.style.left = `${x - cursorWidth / 2}px`; // Ajustement de la position en soustrayant la moitié de la largeur
-        cursor.style.top = `${y}px`;
+    UpdateCursorPosition(x) {
+        this.#gameCursor.style.left = `${x}px`;
     }
 }
