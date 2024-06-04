f68cdff09b4a2845b9059982fea44b5edf217232
yoann <yrebah@webnet.fr>
2024-06-04 18:03:28 +0200

    fix responsive mobile

--- app/dist/js/controllers/Loader.js
+++ app/dist/js/controllers/Loader.js
@@ -7,10 +7,13 @@ class Loader {
     static Show() {
         const loader = Loader.LoaderHTML;
         if (loader) loader.classList.remove('display-none');
+        window.scrollTo(0, 0);
+        document.body.style.overflow = "hidden";
     }
 
     static Hide() {
         const loader = Loader.LoaderHTML;
         if (loader) loader.classList.add('display-none');
+        document.body.style.overflow = "auto";
     }
 }
